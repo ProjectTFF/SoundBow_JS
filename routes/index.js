4a206b259b772f1d74d524726cb187b8a3a2dc4f
Samu Salko <samu.salko@gmail.com>
2022-02-16 18:31:53 +0200

    Implemented sound changing via file uploading

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var path = require("path");
 var formidable = require('formidable');
 var fs = require('fs');
+const { stringify } = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,17 +17,42 @@ router.get('/upload', function(req, res, next) {
   res.sendFile('public/upload.html', { root: '.' });
 });
 
+router.get('/reset_sounds', function(req, res, next) {
+  
+})
+
 router.post('/fileupload', function(req, res, next) {
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
 
+    // If sound is not in .wav-format, throw error
+    try {
+      var old_filename = files.filetoupload.originalFilename;
+      var file_ending = old_filename.substr(old_filename.length - 4);
+      if (file_ending != ".wav") {
+        throw 'Wrong filetype';
+      }
+    }
+    catch(e) {
+      res.write("Things went wrong. Error: " + e);
+      res.end()
+      return;
+    }
     // Upload the new file from somewhere to the sound folder
     var oldpath = files.filetoupload.filepath;
-    var newpath = path.join(__dirname, '../assets/sounds', files.filetoupload.originalFilename);
+    var new_filename = fields.storeplace
+    var newpath = path.join(__dirname, '../assets/sounds/', new_filename);
+    console.log("uusi polku olisi " + newpath);
     fs.rename(oldpath, newpath, function(err) {
-      if (err) throw err;
-      res.write('jotain tapahtui???');
-      res.end();
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      else {
+        res.write('File updated!');
+        res.end();
+      }
+      
     })
   })
 })
